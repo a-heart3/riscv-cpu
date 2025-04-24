a35d9a4e20f9ef7b39187c56a7b26f890410edd1
jinjinjin <17302241931@163.com>
2025-04-24 23:31:39 +0800

    memory has done, need to shipei

--- simsrc/ram_we_tb.v
+++ simsrc/ram_we_tb.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 04/24/2025 08:41:11 PM
+// Create Date: 04/24/20210 08:41:11 PM
 // Design Name: 
 // Module Name: ram_we_tb
 // Project Name: 
@@ -23,6 +23,7 @@
 module ram_we_tb();
 reg [2:0] mode;
 reg [1:0] cs;
+reg       we;
 wire      we0;
 wire      we1;
 wire      we2;
@@ -34,33 +35,39 @@ ram_we ram_we(
     .we0 (we0  ),
     .we1 (we1  ),
     .we2 (we2  ),
-    .we3 (we3  )
+    .we3 (we3  ),
+    .we  (we   )
 );
+
+always begin
+    #5; we = ~we;
+end
 
 initial begin
     mode = 3'b001;
+    we   = 0;
     cs   = 3'b00;
-    #5;
+    #10;
     cs   = 3'b01;
-    #5;
+    #10;
     cs   = 3'b10;
-    #5;
+    #10;
     cs   = 3'b11;
-    #5;
+    #10;
     mode = 3'b010;
-    #5;
+    #10;
     cs   = 3'b10;
-    #5;
+    #10;
     cs   = 3'b01;
-    #5;
+    #10;
     cs   = 3'b00;
-    #5;
+    #10;
     mode = 3'b100;
-    #5;
+    #10;
     cs   = 3'b01;
-    #5;
+    #10;
     cs   = 3'b10;
-    #5;
+    #10;
     cs   = 3'b11;
 end
 endmodule
--- vsrc/ram_we.v
+++ vsrc/ram_we.v
@@ -21,6 +21,7 @@
 
 
 module ram_we(
+    input        we,
     input  [2:0] mode,
     input  [1:0] cs,
     output       we0,
@@ -39,19 +40,23 @@ assign wdata16 = mode[1];
 assign wdata8  = mode[0];
 
 // signal logic
-assign we3 = wdata32
+assign we3 = (wdata32
            | ((cs == 2'b10) & wdata16)
-           | ((cs == 2'b11) & wdata8);
+           | ((cs == 2'b11) & wdata8))
+           & we;
 
-assign we2 = wdata32
+assign we2 = (wdata32
            | ((cs == 2'b10) & wdata16)
-           | ((cs == 2'b10) & wdata8);
+           | ((cs == 2'b10) & wdata8))
+           & we;
 
-assign we1 = wdata32
+assign we1 = (wdata32
            | ((cs == 2'b00) & wdata16)
-           | ((cs == 2'b01) & wdata8);
+           | ((cs == 2'b01) & wdata8))
+           & we;
 
-assign we0 = wdata32
+assign we0 = (wdata32
            | ((cs == 2'b00) & wdata16)
-           | ((cs == 2'b00) & wdata8);           
+           | ((cs == 2'b00) & wdata8))
+           & we;           
 endmodule
